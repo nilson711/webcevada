6980001a9dd03dbedbb729788d3a2dd4366388d9
Nilson Campos <nilson711@gmail.com>
2023-07-10 17:48:28 -0300

    QUERY para Comandas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Pagto;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,27 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        
+        $comandas = DB::SELECT("SELECT V.id, C.nomeClient, V.data, V.valorProdutos, 
+                                COALESCE(SUM(P.valor), 0) AS pgts
+                                 FROM vendas AS V
+                                LEFT JOIN clientes AS C
+                                ON C.id = V.clientes_id
+                               
+                                LEFT JOIN pagtos as P
+                                ON P.venda_id = V.id
+                               
+                                WHERE V.valorProdutos > 0
+                                GROUP BY V.id
+                                HAVING pgts < V.valorProdutos
+
+                                ;");
+
+
+
+        dd($comandas);  
+
+    return view('home', ['comandas' => $comandas]);
+
     }
 }
